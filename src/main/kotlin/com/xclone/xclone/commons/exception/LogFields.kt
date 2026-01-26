3439d9c3978b9499f01cccc7bdd0038718c29b8e
Dave <davidglogowski2003@gmail.com>
2026-01-26 01:50:26 +0100

    logging: Add logger & GlobalExceptionHandler

---
+++ src/main/kotlin/com/xclone/xclone/commons/exception/LogFields.kt
@@ -0,0 +1,8 @@
+package com.xclone.xclone.commons.exception
+
+object LogFields {
+    const val ERROR_CODE = "errorCode"
+    const val RESPONSE_STATUS = "responseStatus"
+    const val URI_PATH = "uriPath"
+    const val FIELD_COUNT = "fieldCount"
+}
